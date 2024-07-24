7831934d6738240a46e0dca1f186febced9ab1cb
Jarek Jeda <jarek.jeda@gmail.com>
2024-07-24 17:24:35 +0200

    remove-vowels

---
+++ homework/remove-vowels/vowel.cpp
@@ -0,0 +1,11 @@
+#include <algorithm>
+
+void removeVowels(std::vector<std::string>& words) {
+    auto isVowel = [](char c) {
+        static const std::string vowels = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+        return vowels.find(c) != std::string::npos;
+    };
+    for (auto& word : words) {
+        text.erase(std::remove_if(word.begin(), word.end(), isVowel), word.end());
+    }
+}
